aab36ba92ff8b41f0036fbf2905737b2df8b9015
Aspect13 <ad13box@gmail.com>
2021-11-12 20:00:30 +0300

    WIP: finishing CRUD sequence for integrations

--- static/js/integrations.js
+++ static/js/integrations.js
@@ -1,169 +1,185 @@
 const Email = {
-    pluginName: 'reporter_email',
-    apiPath: '/api/v1/integrations/reporter_email/',
-    preFetch: () => {
-        clearErrors()
-        $(`#${Email.pluginName}_test_connection`).addClass('updating').addClass('disabled')
-        $(`#${Email.pluginName}_submit`).addClass('updating').addClass('disabled')
-    },
-    postFetch: () => {
-        $(`#${Email.pluginName}_test_connection`).removeClass('updating').removeClass('disabled')
-        $(`#${Email.pluginName}_submit`).removeClass('updating').removeClass('disabled')
-    },
-    handleError: response => response.json().then(
-        errorData => {
-            console.log(errorData)
-            errorData.forEach(item => {
-                // const errorId = `error_qualys_${item.loc[0]}`
-                // console.log(errorId)
-                $(`#${Email.pluginName}_${item.loc[0]}`).addClass('is-invalid')
-                // $(`#error_qualys_${item.loc[0]}`).text(item.msg).show()
-            })
-        }
-    ),
-    create: () => {
-        const url = $('#qualys_url').val()
-        const login = $('#qualys_login').val()
-        const passwd = $('#qualys_password').val()
-        const description = $('#qualys_description').val()
-        const is_default = $('#qualys_default').prop('checked')
-        const project_id = getSelectedProjectId()
-        Email.preFetch()
-        fetch(Email.apiPath, {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({url, login, passwd, project_id, description, is_default})
-        }).then(response => {
-            console.log(response)
-            Email.postFetch()
-            if (response.ok) {
-                // $('#qualys_submit').removeClass('disabled')
-                // $('#qualys_test_connection').removeClass('btn-warning').addClass('btn-success').removeClass('btn-secondary')
-                $('#qualys_integration').modal('hide')
-                location.reload()
-            } else {
-                Email.handleError(response)
-            }
-        })
-    },
-    testConnection: () => {
-        const url = $('#qualys_url').val()
-        const login = $('#qualys_login').val()
-        const passwd = $('#qualys_password').val()
-        Email.preFetch()
-        $('#qualys_test_connection').removeClass('btn-success').addClass('btn-secondary')
-        fetch(apiPath + 'check_settings', {
-            method: 'POST',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({url, login, passwd})
-        }).then(response => {
-            console.log(response)
-            Email.postFetch()
-            if (response.ok) {
-                // $('#qualys_submit').removeClass('disabled')
-                $('#qualys_test_connection').removeClass('btn-warning').removeClass('btn-secondary').addClass('btn-success')
-                // $('#qualys_integration').modal('hide')
-                // location.reload()
-            } else {
-                $('#qualys_test_connection').removeClass('btn-success').removeClass('btn-secondary').addClass('btn-warning')
-                Email.handleError(response)
-            }
-        })
-    },
-    clearErrors: () => {
-        $('#qualys_url').removeClass('is-invalid')
-        $('#qualys_login').removeClass('is-invalid')
-        $('#qualys_password').removeClass('is-invalid')
-        $('#error_qualys_url').hide()
-        $('#error_login_url').hide()
-        $('#error_password_url').hide()
-        $('#error_qualys_check_connection').hide()
-        $('#qualys_test_connection').removeClass('btn-warning').removeClass('btn-success').addClass('btn-secondary').removeClass('updating')
-        $('#qualys_submit').removeClass('updating')
-    },
-    clearForm: () => {
-        $('#qualys_url').val('')
-        $('#qualys_login').val('')
-        $('#qualys_description').val('Qualys default')
-        $('#qualys_default').prop('checked', false)
-        $('#qualys_password').val('').prop('readonly', false).attr('placeholder', 'Password')
-        $('#qualys_test_connection').removeClass('updating')
-        $('#qualys_submit').removeClass('updating')
-    },
-    update: cardData => {
-        console.log('UPDATE', cardData)
-        const url = $('#qualys_url').val()
-        const login = $('#qualys_login').val()
-        const passwd = $('#qualys_password').prop('readonly') ? null : $('#qualys_password').val()
-        const description = $('#qualys_description').val()
-        const is_default = $('#qualys_default').prop('checked')
-        Email.preFetch()
-        fetch(Email.apiPath + cardData.id, {
-            method: 'PUT',
-            headers: {'Content-Type': 'application/json'},
-            body: JSON.stringify({url, login, passwd, is_default, description})
-        }).then(response => {
-            console.log(response)
-            Email.postFetch()
-            if (response.ok) {
-                // $('#qualys_submit').removeClass('disabled')
-                // $('#qualys_test_connection').removeClass('btn-warning').addClass('btn-success').removeClass('btn-secondary')
-                // $('#qualys_integration').modal('hide')
-                location.reload()
-            } else {
-                Email.handleError(response)
-            }
-        })
-    },
     edit: data => {
         console.log('editIntegration', data)
-        const {settings: {url, login}, description, is_default} = data;
-        $('#qualys_url').val(url)
-        $('#qualys_login').val(login)
-        const pass = $('#qualys_password')
-        pass.prop('readonly', true).attr('placeholder', 'Click to change')
-        pass.on('click', () => pass.prop('readonly', false))
-        $('#qualys_description').val(description)
-        $('#qualys_default').prop('checked', is_default)
-
-        $('#qualys_submit').on('click', Email.update.bind(null, data))
-        $('#qualys_integration').modal('show')
+        const {description, is_default, id, settings} = data
+        emailVm.load({...settings, description, is_default, id})
+        emailVm.modal.modal('show')
     },
     delete: id => {
-        console.log('deleteIntegration', id)
-        fetch(apiPath + id, {
-            method: 'DELETE',
-        }).then(response => {
-            console.log(response)
-            postFetch()
-            if (response.ok) {
-                location.reload()
-            } else {
-                handleError(response)
-            }
-        })
+        emailVm.load({id})
+        emailVm.delete()
     }
-
-
-
 }
 
+const emailInitialState = () => ({
+    host: '',
+    port: null,
+    user: '',
+    password: '',
+    sender: '',
+    description: '',
+    is_default: false,
+    is_fetching: false,
+    error: {},
+    test_connection_status: 0,
+    id: null
+})
 
+const emailApp = Vue.createApp({
+    delimiters: ['[[', ']]'],
+    // props: [
+    //     'host', 'port', 'user',
+    //     'password', 'sender', 'description',
+    //     'is_default', 'is_fetching', 'error',
+    //     'test_connection_status', 'id'
+    // ],
+    data() {
+        return {
+            pluginName: 'reporter_email',
+            modal: $('#reporter_email_integration'),
+            ...emailInitialState()
+        }
+    },
+    mounted() {
+        console.log('mounted', this.$el)
+        this.modal.on('hidden.bs.modal', e => {
+            this.clear()
+        })
+    },
+    computed: {
+        apiPath() {
+            return `/api/v1/integrations/${this.pluginName}/`
+        },
+        project_id() {
+            return getSelectedProjectId()
+        },
+        body_data() {
+            const {host, port, user, password: passwd, sender, description, is_default, project_id} = this
+            return {host, port, user, passwd, sender, description, is_default, project_id}
+        },
+        test_connection_class() {
+            if (200 <= this.test_connection_status && this.test_connection_status < 300) {
+                return 'btn-success'
+            } else if (this.test_connection_status > 0) {
+                return 'btn-warning'
+            } else {
+                return 'btn-secondary'
+            }
+        }
+    },
+    watch: {
+        is_fetching(newState, oldState) {
+            if (newState) {
+                this.test_connection_status = 0
+            }
+        }
+    },
+    methods: {
+        test_connection() {
+            console.log('TEST CONN', this.$data)
+            this.is_fetching = true
+            $('#qualys_test_connection').removeClass('btn-success').addClass('btn-secondary')
+            fetch(this.apiPath + 'check_settings', {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(this.body_data)
+            }).then(response => {
+                console.log(response)
+                this.is_fetching = false
+                this.test_connection_status = response.status
+                if (!response.ok) {
+                    this.handleError(response)
+                }
+            })
+        },
+        clear() {
+            Object.assign(this.$data, {
+                ...this.$data,
+                ...emailInitialState(),
+            })
+        },
+        load(stateData) {
+            Object.assign(this.$data, {
+                ...this.$data,
+                ...stateData,
+                pluginName: 'reporter_email',
+                modal: $('#reporter_email_integration'),
+            })
+        },
+        create() {
+            this.is_fetching = true
+            fetch(this.apiPath, {
+                method: 'POST',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(this.body_data)
+            }).then(response => {
+                console.log(response)
+                this.is_fetching = false
+                if (response.ok) {
+                    this.modal.modal('hide')
+                    alertMain.add('Email reporter created!', 'success-overlay')
+                    setTimeout(() => location.reload(), 2000)
+                } else {
+                    this.handleError(response)
+                }
+            })
+        },
+        handleError(response) {
+            try {
+                response.json().then(
+                    errorData => {
+                        console.log(errorData)
+                        errorData.forEach(item => {
+                            console.log('item error', item)
+                            this.error = {[item.loc[0]]: item.msg}
+                        })
+                    }
+                )
+            } catch (e) {
+                alertMain.add(e, 'danger-overlay')
+            }
 
-$(document).ready(function () {
+        },
+        update() {
+            console.log('update')
+            this.is_fetching = true
+            fetch(this.apiPath + this.id, {
+                method: 'PUT',
+                headers: {'Content-Type': 'application/json'},
+                body: JSON.stringify(this.body_data)
+            }).then(response => {
+                console.log(response)
+                this.is_fetching = false
+                if (response.ok) {
+                    this.modal.modal('hide')
+                    alertMain.add('Email reporter updated!', 'success-overlay')
+                    setTimeout(() => location.reload(), 2000)
+                } else {
+                    this.handleError(response)
+                }
+            })
+        },
+        delete() {
+            console.log('deleteIntegration')
+            this.is_fetching = true
+            fetch(this.apiPath + this.id, {
+                method: 'DELETE',
+            }).then(response => {
+                console.log(response)
+                this.is_fetching = false
+                if (response.ok) {
+                    alertMain.add('Email integration deleted')
+                    setTimeout(() => location.reload(), 2500)
+                } else {
+                    this.handleError(response)
+                    alertMain.add(`Deletion error. <button class="btn btn-primary" @click="modal.modal('show')">Open modal<button>`)
+                }
+            })
+        }
+    }
 
-    // $('#create_integration_qualys').on('click', () => $('#qualys_submit').on('click', qualysCreate));
-    // $('#qualys_test_connection').on('click', qualysTestConnection);
-    // // $('#qualys_url').on('change', () => $('#qualys_submit').addClass('disabled'))
-    // // $('#qualys_login').on('change', () => $('#qualys_submit').addClass('disabled'))
-    // // $('#qualys_password').on('change', () => $('#qualys_submit').addClass('disabled'))
-    // // $('#qualys_integration').on('show.bs.modal', e => {
-    // //     $('#qualys_submit').addClass('disabled')
-    // // })
-    // $('#qualys_integration').on('hidden.bs.modal', e => {
-    //     clearErrors();
-    //     clearForm();
-    //     $('#qualys_submit').prop("onclick", null).off("click");
-    //     console.log('clearing click')
-    // })
-});
+})
+emailApp.config.compilerOptions.isCustomElement = tag => ['h9', 'h13'].includes(tag)
+
+const emailVm = emailApp.mount('#reporter_email_integration')
+
